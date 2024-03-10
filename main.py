b361acfa97bb90863f0617167360fa072fefc53e
Harshana Karunarathna <95669599+HarshanaLK@users.noreply.github.com>
2024-03-10 14:16:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,4 +81,4 @@ async def detect_emotion_route(image: UploadFile = Form(...)):
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run(app,host="0.0.0.0", port=5000)
+    uvicorn.run(app,host="0.0.0.0", port=8000)
